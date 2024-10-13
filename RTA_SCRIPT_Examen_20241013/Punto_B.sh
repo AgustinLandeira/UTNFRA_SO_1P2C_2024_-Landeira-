09f92a2d2de085bbca89e4cd8ae3e7ea450df962
agus2023 <vagrant@parcial>
2024-10-13 23:08:18 +0000

    Feat:montando de forma persistente en el punto b

--- RTA_SCRIPT_Examen_20241013/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241013/Punto_B.sh
@@ -65,3 +65,12 @@ sudo mkfs.ext4 /dev/sdc10
 lsblk
 sudo lsblk -f
 
+sudo mount /dev/sdc1 /Examenes-UTN/alumno_1/parcial_1
+sudo mount /dev/sdc2 /Examenes-UTN/alumno_1/parcial_2
+sudo mount /dev/sdc3 /Examenes-UTN/alumno_1/parcial_3
+sudo mount /dev/sdc5 /Examenes-UTN/alumno_2/parcial_1
+sudo mount /dev/sdc6 /Examenes-UTN/alumno_2/parcial_2
+sudo mount /dev/sdc7 /Examenes-UTN/alumno_2/parcial_3
+sudo mount /dev/sdc8 /Examenes-UTN/alumno_3/parcial_1
+sudo mount /dev/sdc9 /Examenes-UTN/alumno_3/parcial_2
+sudo mount /dev/sdc10 /Examenes-UTN/alumno_3/parcial_3

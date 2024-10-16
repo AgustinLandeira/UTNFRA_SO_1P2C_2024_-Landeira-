e4830ffbf66be7fd03cabfc9504984fb885ea8da
agus2023 <vagrant@parcial>
2024-10-16 13:27:45 +0000

    Feat_ cambiando los directorios para completar el punto E

--- RTA_SCRIPT_Examen_20241013/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241013/Punto_E.sh
@@ -1,4 +1,4 @@
 grep MemTotal /proc/meminfo
-grep MemTotal /proc/meminfo > repogit/UTN-FRA_SO_Examenes/Filtro_Basico.txt
+grep MemTotal /proc/meminfo > repogit/UTNFRA_SO_1P2C_2024_-Landeira-/RTA_ARCHIVOS_Examen_20241013/Filtro_Basico.txt
 sudo dmidecode -t chassis
-sudo dmidecode -t chassis | grep -A1 "Chassis Information" >> repogit/UTN-FRA_SO_Examenes/Filtro_Basico.txt
+sudo dmidecode -t chassis | grep -A1 "Chassis Information" >> repogit/UTNFRA_SO_1P2C_2024_-Landeira-/RTA_ARCHIVOS_Examen_20241013/Filtro_Basico.txt
